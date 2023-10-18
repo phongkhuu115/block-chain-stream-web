e7dabbbd116e83b95948320f742c5f99791e7f63
Nguyen Anh Tai <20520924@gm.uit.edu.vn>
2023-10-19 01:53:58 +0700

    feat(header): raw header

---
+++ front-end/src/common/icons/underline.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+type Props = {};
+
+const Underline = (props: Props) => {
+  return (
+    <svg
+      width="74"
+      height="6"
+      viewBox="0 0 74 6"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3 3L71.25 3"
+        stroke="white"
+        stroke-width="5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default Underline;
